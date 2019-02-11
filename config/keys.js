07ae542c6885a4148a75aa28418fad9f6a55fcea
AnexHeil <humanstudio@mail.ru>
2019-02-11 15:42:07 +0300

    initial commit

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+  mongoURI: 'mongodb://Heil:135928a@ds129625.mlab.com:29625/storybooks-dev',
+  googleClientID: '350749177824-0vrogc74arsk5dccfc0pbeie8hhhh2d3.apps.googleusercontent.com',
+  googleClientSecret: 'nH5SrJrBmuGDqj_hq8eRhR94'
+}
